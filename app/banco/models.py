93be7b40788a48d819b467f7693a805c51271c44
Davi-Ga <davigaldinoky@gmail.com>
2022-05-04 15:13:52 -0300

    models

--- app/banco/models.py
+++ app/banco/models.py
@@ -1,22 +1,45 @@
-from django.db import models
+from banco import db
 
 
-class Agencia(models.Model):
-    id = models.AutoField(primary_key=True,null=False)
-    id_banco = models.IntegerField(null=False)
-    endereco = models.CharField(max_length=250)
-    fone = models.BigIntegerField()
-    tipo = models.IntegerField()
-    fone1 = models.BigIntegerField(blank=True)
-    tipo1 = models.IntegerField(blank=True)
-    agencia = models.CharField(max_length=250)
-    nome_agencia = models.CharField(max_length=250)
+class Banco(db.Model):
+    __tablename__='banco'
     
-class Banco(models.Model):
-    id = models.AutoField(primary_key=True,null=False)
-    nome = models.CharField(max_length=250,null=False)
-    numero = models.CharField(max_length=250)
+    id=db.Column(db.Integer,primary_key=True,nullable=False,autoincrement=True)
+    nome=db.Column(db.String(200))
+    numero=db.Column(db.String(200))
+
+    def __init__(self,nome,numero):
+        self.nome=nome
+        self.numero=numero
+        
+class Agencia(db.Model):
+    TIPO_PHONE=[
+        (u'0',u'Celular'),
+        (u'1',u'Fixo'),
+    ]
+    __tablename__='agencia'
+
+    id=db.Column(db.Integer,primary_key=True,nullable=False,autoincrement=True)
+    id_banco=db.Column(db.Integer, db.ForeignKey('Banco.id'),nullable=False)
+    endereco=db.Column(db.String(200))
+    tipo=db.Column(ChoiceType=(TIPO_PHONE))
+    fone=db.Column(db.BigInteger)
+    tipo1=db.Column(db.Integer,nullable=True)
+    fone1=db.Column(db.BigInteger,nullable=True)
+    agencia=db.Column(db.String(200))
+    nome_agencia=db.Column(db.String(200))
     
-def __str__(self):
-        return self.description
-    
+    def __init__(self, id_banco,endereco,tipo,fone,tipo1,fone1,agencia,nome_agencia):
+        self.id_banco=id_banco
+        self.endereco=endereco
+        self.tipo=tipo
+        self.fone=fone
+        self.tipo1=tipo1
+        self.fone1=fone1
+        self.agencia=agencia
+        self.nome_agencia=nome_agencia
+  
+agencia=type      
+
+               
+db.create_all()
