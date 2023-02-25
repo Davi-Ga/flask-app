afa5400d822cf84e5b29a545599641f756388355
Davi-Ga <davigaldinoky@gmail.com>
2023-02-25 20:52:02 -0300

    ajustes

--- app/banco/views.py
+++
@@ -1,7 +0,0 @@
-from re import template
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.views.generic import TemplateView
-
-class PaginaInicial(TemplateView):
-    template_name = 'banco/index.html'
